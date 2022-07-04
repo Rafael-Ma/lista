5c4d6b9212d15a89b0f6b5a7618052ab31d1c5a0
Rafael <pessoalrafamt@gmail.com>
2022-07-04 18:27:27 +0300

    Novos exercícios

---
+++ exer10.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class exer10 {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)){
+            // Atributos
+            int idade;
+            
+            // Input
+            System.out.println("Informe a idade: ");
+            idade = input.nextInt();
+
+            // Condições
+
+        }
+    }   
+}
